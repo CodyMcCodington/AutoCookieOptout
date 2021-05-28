93351bb4416eb26f7ff5c6f35d15ef8b3e3bbcdd
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-05-28 17:59:19 +0200

    Add international language support
    These identifiers don't rely on English being the user's language.

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,10 +1,10 @@
 if (document.location.pathname === '/m') {
     // Page 1
-    document.querySelector('a[aria-label="Customize"]').click();
+    document.querySelector('a[href^="https://consent.youtube.com/d?"]').click();
 } else if (document.location.pathname === '/d') {
     // Page 2
-    document.querySelector('button[aria-label="Turn off Search customization"]').click();
-    document.querySelector('button[aria-label="Turn off YouTube History"]').click();
-    document.querySelector('button[aria-label="Turn off Ad personalization"]').click();
+    document.querySelector('[jsname=yUNjVb]').click();
+    document.querySelector('[jsname=FXYDXd]').click();
+    document.querySelector('[jsname=SHqtNc]').click();
     document.querySelector('form').submit();
 }
