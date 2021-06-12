f3059cc82cf2dae328cfec70ab3e8d339745c2e4
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-06-12 17:25:39 +0200

    Support LiveRamp's consent framework

---
+++ src/clickers/liveramp.ts
@@ -0,0 +1,10 @@
+import { clickAllElements, clickWhenFound } from "./common";
+
+(async function() {
+    await clickWhenFound('button#manageSettings');
+    clickAllElements('.items [aria-checked=true]')
+    await clickWhenFound('button#saveAndExit');
+
+    // Some websites require you to confirm saving your settings if you don't opt in to everything
+    clickAllElements('.confirmation-dialog .okButton');
+})();
