4c3c5d7f42b6d35c219d0820957ab30245bde329
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-06-12 17:30:11 +0200

    Streamline common logic
    - Improve logging consistency
    - Improve code reuse

--- src/clickers/common.ts
+++ src/clickers/common.ts
@@ -13,6 +13,7 @@ function attachScriptToBodyLoad(scriptElement: HTMLScriptElement) {
 function clickElement(selector: string, ignoreIfNotPresent = false) {
     const element = document.querySelector(selector);
     if (element instanceof HTMLElement) {
+        console.debug(`Clicking ${selector}`);
         element.click();
     } else if (!element && ignoreIfNotPresent) {
         return;
@@ -32,37 +33,22 @@ function clickAllElements(selector: string) {
 }
 
 async function clickWhenFound(selector: string) {
+    await waitUntilFound(selector);
+    clickElement(selector);
+}
+
+async function clickAllWhenFound(selector: string) {
+    await waitUntilFound(selector);
+    clickAllElements(selector);
+}
+
+async function waitUntilFound(selector: string) {
     return new Promise<void>((resolve) => {
         function callback(document: Document) {
             console.debug(`Checking for ${selector}`);
             if (document.querySelector(selector)) {
                 console.debug(`Found ${selector}`);
                 observer.disconnect();
-                clickElement(selector);
-                resolve();
-            }
-        }
-        const observer = new MutationObserver(() => {
-            callback(document);
-        });
-
-        observer.observe(document.querySelector('html'), {
-            subtree: true,
-            childList: true,
-            attributes: true,
-        });
-        callback(document);
-    })
-}
-
-async function clickAllWhenFound(selector: string) {
-    return new Promise<void>((resolve) => {
-        function callback(document: Document) {
-            console.debug(`Checking for all matching ${selector}`);
-            if (document.querySelector(selector)) {
-                console.debug(`Found ${selector}`);
-                observer.disconnect();
-                clickAllElements(selector);
                 resolve();
             }
         }
