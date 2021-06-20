ec4f57b13ce14d4b93347f6cf28a7efd3ff86391
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-06-20 11:40:02 +0200

    Sort vendors

--- src/indirectClickers/vendors.ts
+++ src/indirectClickers/vendors.ts
@@ -2,21 +2,21 @@
 // URLs are requested and then messaging the tab that did the request
 enum TriggeredVendor {
     CookieBot = 'cookiebot',
+    CookiePro = 'cookiepro',
     Consensu = 'consensu',
     Didomi = 'didomi',
-    CookiePro = 'cookiepro',
 };
 
 const triggeredVendorPatternLists: Record<TriggeredVendor, string[]> = Object.freeze({
     [TriggeredVendor.CookieBot]: ["https://consent.cookiebot.com/uc.js"],
+    [TriggeredVendor.CookiePro]: [
+        "https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js",
+        "https://cdn.cookielaw.org/scripttemplates/otSDKStub.js",
+    ],
     [TriggeredVendor.Consensu]: ["https://quantcast.mgr.consensu.org/choice/*"],
     [TriggeredVendor.Didomi]: [
         "https://sdk.privacy-center.org/loader.js?*",
         "https://sdk.privacy-center.org/*/loader.js?*",
-    ],
-    [TriggeredVendor.CookiePro]: [
-        "https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js",
-        "https://cdn.cookielaw.org/scripttemplates/otSDKStub.js",
     ],
 });
 
