d112c9b8c9490056e006430cdb1c2c4744d977a8
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-06-27 22:19:41 +0200

    Expand Sourcepoint to more websites

--- src/directClickers/sourcepoint.ts
+++ src/directClickers/sourcepoint.ts
@@ -1,18 +1,30 @@
-import { clickAllWhenFound, clickWhenFound } from "../common";
+import { clickAllElements, clickAllWhenFound, clickElement, clickWhenFound, retryUntil, untilStable } from "../common";
 import { log } from "../logger";
 
 (async function() {
     if (document.location.pathname === '/index.html') {
         log('Detected Sourcepoint intro screen');
-        await clickWhenFound('[aria-label="Options"]');
+
+        // Sourcepoint selectors are quite fuzzy due to variations and lack of language-independent
+        // characteristics
+        await clickWhenFound('.message-container .pg-configure-button, [aria-label="Options"],  [title="Options"]');
     } else if (document.location.pathname === '/privacy-manager/index.html') {
         log('Detected Sourcepoint options screen');
+        await untilStable(200);
 
-        // Explicitly opt out of legitimate interest items (enabled by default)
-        await clickWhenFound('[aria-label="Legitimate Interest"]');
-        await clickAllWhenFound('.pm-switch.checked .slider');
-        await clickWhenFound('[aria-label="Save & Exit"]');
-        log('Sourcepoint popup handled')
+        // Opt out of anything that's enabled by default
+        clickAllElements('.stack-toggle.reject-toggle, .pm-switch.checked .slider');
+
+        // Explicitly opt out of legitimate interest items if the tab is present (enabled by default)
+        const legitInterestSelector = '[aria-label="Legitimate Interest"], [title="Legitimate Interest"]';
+        if (document.querySelector(legitInterestSelector)) {
+            log('Found a legitimate interest tab, opting out of checked items')
+            await clickWhenFound(legitInterestSelector);
+            await clickAllWhenFound('.pm-switch.checked .slider');
+        }
+
+        await clickWhenFound('.message-container .pm-preferences-button, [aria-label="Save & Exit"], [title="Save & Exit"]');
+        log('Sourcepoint popup handled');
     } else {
         throw new Error('Did not match with any paths');
     }
