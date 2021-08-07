8f6ce0d5fac4d7533706383369d87de73ff0d2b1
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-08-07 20:04:18 +0200

    Fix Quantcast behavior
    Sometimes a 'Reject all' button is present in front of the advanced
    settings button. This may cause ACO to click that button and assume
    further steps are needed. When that button is present, skip all other
    steps.

--- src/indirectClickers/quantcast.ts
+++ src/indirectClickers/quantcast.ts
@@ -1,9 +1,22 @@
-import { clickWhenFound } from "../common";
+import { clickWhenFound, clickWhenOneOfFollowingFound } from "../common";
+import { log } from "../logger";
 
 async function handle() {
-    await clickWhenFound('.qc-cmp2-summary-buttons button[mode=secondary]');
-    await clickWhenFound('.qc-cmp2-header-links button[aria-label="REJECT ALL"]');
-    await clickWhenFound('.qc-cmp2-footer button[aria-label="SAVE & EXIT"]');
+    const fallbackSelector = '.qc-cmp2-summary-buttons button[mode=secondary]';
+
+    // Sometimes Quantcast displays the optout button right away. Other times
+    // we may have to go into advanced settings to opt out.
+    const foundSelector = await clickWhenOneOfFollowingFound([
+        '.qc-cmp2-summary-buttons button[aria-label="DISAGREE"]',
+        fallbackSelector,
+    ]);
+
+    if (foundSelector === fallbackSelector) {
+        log('Rejecting everything from the settings page');
+        await clickWhenFound('.qc-cmp2-header-links button[aria-label="REJECT ALL"]');
+        await clickWhenFound('.qc-cmp2-footer button[aria-label="SAVE & EXIT"]');
+    }
+    log('Quantcast popup dismissed');
 }
 
 export default handle;
