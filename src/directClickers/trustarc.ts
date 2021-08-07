51e847e83f24b6ec4af6e47b98f2220d4edd4c3a
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-08-07 17:58:23 +0200

    Improve TrustArc coverage
    - This should fix issue #7
    - Add some extra utility methods that provide additional flexibility to
    clickers, allowing them to react differently depending on which element
    was clicked

--- src/directClickers/trustarc.ts
+++ src/directClickers/trustarc.ts
@@ -1,4 +1,4 @@
-import { clickAllWhenFound, clickWhenFound } from "../common";
+import { clickAllWhenFound, clickWhenFound, clickWhenOneOfFollowingFound } from "../common";
 import { log } from "../logger";
 
 (async function() {
@@ -6,27 +6,30 @@ import { log } from "../logger";
 
     // Standalone variant where everything happens in an iframe
     if (document.location.pathname === '/' && searchParams.get('layout') === 'default_eu') {
-        await clickWhenFound('.shp');
-
-        // TrustArc's submit button is a little bit buggy. Sometimes it breaks and outputs
-        // a stack trace in the console. Workaround: if at first you don't succeed, dust
-        // yourself off and try again.
-        let formCloseTries = 0;
-        async function submitForm() {
-            await clickWhenFound('.pdynamicbutton .submit');
-            formCloseTries++;
-
-            // The body turns itself invisible when the preferences have been submitted
-            if (document.querySelector('body').style.display !== 'none') {
-                if (formCloseTries < 10) {
-                    setTimeout(submitForm, 50);
-                } else {
-                    log('Cookie popup may still be open due to vendor quirks')
+        const requiredOnlySelector = 'a.required';
+        const changePreferencesSelector = 'a.shp';
+        const matchedSelector = await clickWhenOneOfFollowingFound([requiredOnlySelector, changePreferencesSelector]);
+        
+        if (matchedSelector === changePreferencesSelector) {
+            // TrustArc's submit button is a little bit buggy. Sometimes it breaks and outputs
+            // a stack trace in the console. Workaround: if at first you don't succeed, dust
+            // yourself off and try again.
+            let formCloseTries = 0;
+            async function submitForm() {
+                await clickWhenFound('.pdynamicbutton .submit');
+                formCloseTries++;
+    
+                // The body turns itself invisible when the preferences have been submitted
+                if (document.querySelector('body').style.display !== 'none') {
+                    if (formCloseTries < 10) {
+                        setTimeout(submitForm, 50);
+                    } else {
+                        log('Cookie popup may still be open due to vendor quirks')
+                    }
                 }
             }
+            submitForm();
         }
-        submitForm();
-        log('TrustArc popup handled');
     }
     // Variant that is initiated through Trustarc's indirect clicker which creates an
     // iframe we have to hook in to directly
