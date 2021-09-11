8323fda093176a7580e39ce25426d7e0adfe3976
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-09-11 18:05:42 +0200

    Add error logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,11 @@ function log(message: string) {
     console.debug(`%cACO 🛡 %c${message}`, "color: green", "color: initial");
 }
 
+function logError(message: string) {
+    console.error(`%cACO 🛡 %c${message}`, "color: red", "color: initial");
+}
+
 export {
     log,
+    logError,
 }
