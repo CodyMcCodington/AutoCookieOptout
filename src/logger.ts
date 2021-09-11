3425430d8bbd8b728c85981741195397aa8f473b
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-09-11 20:27:11 +0200

    Make click logging stand out

--- src/logger.ts
+++ src/logger.ts
@@ -2,11 +2,16 @@ function log(message: string) {
     console.debug(`%cACO 🛡 %c${message}`, "color: green", "color: initial");
 }
 
+function logClick(selector: string) {
+    console.debug(`%cACO 🛡 %cClicking '%c${selector}'`, "color: green", "color: initial", "color: purple");
+}
+
 function logError(message: string) {
     console.error(`%cACO 🛡 %c${message}`, "color: red", "color: initial");
 }
 
 export {
     log,
+    logClick,
     logError,
 }
