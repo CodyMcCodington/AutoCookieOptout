330d9d4a5e6610fe35a4dadc91ff05605a8caf18
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2021-11-22 18:08:24 +0100

    Handle cookiebot special case
    Worked: more than 1 optional category was preselected as enabled
    Didn't work but fixed: no optional category was preselected as enabled

--- src/indirectClickers/cookiebot.ts
+++ src/indirectClickers/cookiebot.ts
@@ -1,20 +1,22 @@
-import { clickAllWhenFound, clickWhenFound, hasCookie, untilStable } from "../common";
+import { clickAllElements, clickAllWhenFound, clickWhenFound, hasCookie, untilOneOfFollowingFound, untilStable } from "../common";
 import { log } from "../logger";
 
 async function handle() {
-    const initialSelector = 'input.CybotCookiebotDialogBodyLevelButton:checked:not(:disabled)';
+    const toggleSelector = 'input.CybotCookiebotDialogBodyLevelButton';
+    const enabledTogglesSelector = `${toggleSelector}:checked:not(:disabled)`;
 
     // If a consent cookie is present, wait a bit longer in case a consent reprompt might be
     // forthcoming. Only take action if needed.
     if (hasCookie('CookieConsent')) {
         await untilStable(1000);
-        if (!document.querySelector(initialSelector)) {
+        if (!document.querySelector(toggleSelector)) {
             log('Cookiebot cookie detected, assuming already opted out');
             return;
         }
     }
 
-    await clickAllWhenFound(initialSelector);
+    await untilOneOfFollowingFound([toggleSelector]);
+    clickAllElements(enabledTogglesSelector);
     await clickWhenFound('#CybotCookiebotDialogBodyLevelButtonAccept');
 }
 
