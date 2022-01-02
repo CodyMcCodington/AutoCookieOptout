42e8fe7c85520c3c4911ec2ae9181efdb6116dd8
CodyMcCodington <84873117+CodyMcCodington@users.noreply.github.com>
2022-01-02 18:35:07 +0100

    Handle more CookieBot cases
    Sample case: wasabi.com
    
    Couldn't regression test the existing cases. We do'nt remember any sites
    that still use it.

--- src/indirectClickers/cookiebot.ts
+++ src/indirectClickers/cookiebot.ts
@@ -1,7 +1,8 @@
-import { clickAllElements, clickAllWhenFound, clickWhenFound, hasCookie, untilOneOfFollowingFound, untilStable } from "../common";
+import { clickAllElements, clickAllWhenFound, clickElement, clickWhenFound, hasCookie, untilOneOfFollowingFound, untilStable } from "../common";
 import { log } from "../logger";
 
 async function handle() {
+    const declineSelector = '#CybotCookiebotDialogBodyButtonDecline';
     const toggleSelector = 'input.CybotCookiebotDialogBodyLevelButton';
     const enabledTogglesSelector = `${toggleSelector}:checked:not(:disabled)`;
 
@@ -15,9 +16,17 @@ async function handle() {
         }
     }
 
-    await untilOneOfFollowingFound([toggleSelector]);
-    clickAllElements(enabledTogglesSelector);
-    await clickWhenFound('#CybotCookiebotDialogBodyLevelButtonAccept');
+    const foundSelector = await untilOneOfFollowingFound([declineSelector, toggleSelector]);
+    if (foundSelector === declineSelector) {
+        // Easy way out this button may be invisible on screen but present in the DOM
+        clickElement(declineSelector);
+    } else if (foundSelector === toggleSelector) {
+        // The design with the checkboxes in the popup
+        clickAllElements(enabledTogglesSelector);
+        await clickWhenFound('#CybotCookiebotDialogBodyLevelButtonAccept');
+    } else {
+        throw new Error('No matching UI found');
+    }
 }
 
 export default handle;
